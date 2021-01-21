7d6084211d00513cf1b32fc04969600684423bdc
高敏 <gaom@vanytech.com>
2021-01-21 16:18:59 +0800

    login.register

--- src/main/java/com/xiyiwe/xichat/XichatApplication.java
+++ src/main/java/com/xiyiwe/xichat/XichatApplication.java
@@ -1,9 +1,11 @@
 package com.xiyiwe.xichat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.xiyiwe.xichat.dao")
 public class XichatApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/xiyiwe/xichat/controller/login/LoginController.java
+++ src/main/java/com/xiyiwe/xichat/controller/login/LoginController.java
@@ -1,13 +1,42 @@
 package com.xiyiwe.xichat.controller.login;
 
+import com.xiyiwe.xichat.config.annotation.DecodeParamter;
+import com.xiyiwe.xichat.config.annotation.EncodeResult;
+import com.xiyiwe.xichat.dao.user.UserMapper;
+import com.xiyiwe.xichat.pojo.login.LoginInfo;
+import com.xiyiwe.xichat.pojo.user.User;
+import com.xiyiwe.xichat.utils.secret.AESCoder;
+import com.xiyiwe.xichat.utils.secret.AesUtil;
+import com.xiyiwe.xichat.utils.secret.desc.DescUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@RestController
 public class LoginController {
-    @PostMapping("/login")
-    String login(String userAccount,String password){
-
-        return "index";
+    @Autowired
+    UserMapper userMapper;
+    @RequestMapping(value = "/login", produces = "application/json;charset=UTF-8")
+    String login(@RequestBody LoginInfo loginInfo) throws Exception {
+        System.out.println(loginInfo);
+//        System.out.println(AesUtil.aesDecrypt(loginInfo.getUserAccount(),"1234567890ABCDEF"));
+//        System.out.println(AesUtil.aesDecrypt(loginInfo.getPassword(),"1234567890ABCDEF"));
+        User rightUser = userMapper.selectByUserAccount(AesUtil.aesDecrypt(loginInfo.getUserAccount(), "1234567890ABCDEF"));
+        if(rightUser!=null){
+            if(DescUtil.decrypt(rightUser.getPassword()).equals(AesUtil.aesDecrypt(loginInfo.getPassword(),"1234567890ABCDEF"))){
+                return "ok";
+            }else{
+                return "密码错误";
+            }
+        }else{
+            return "用户名不存在";
+        }
+//        AESCoder aesCoder = new AESCoder();
+//        aesCoder.decrypt(loginInfo.getUserAccount());
+//        aesCoder.decrypt(loginInfo.getPassword());
     }
 }
